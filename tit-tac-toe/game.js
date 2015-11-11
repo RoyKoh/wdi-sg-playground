5e3a218c66863b45da9e2eb093b675f4155f7d7c
RoyKoh <i168168@yahoo.com.sg>
2015-11-11 17:20:02 +0800

    Modi Delete game.js game.dist.js

--- tit-tac-toe/game.js
+++ tit-tac-toe/game.js
@@ -1,8 +1,6 @@
 var currentPlayer = 'one'
 var body = document.querySelector('body')
 // declare var for winning X and winning O
-var winningX = [['ax','dx','gx'],['bx','ex','hx'],['cx','fx','ix'],['ax','bx','cx'],['dx','ex','fx'],['gx','hx','ix'],['ax','ex','ix'],['gx','ex','cx']];
-var winningO = [['ao','do','go'],['bo','eo','ho'],['co','fo','io'],['ao','bo','co'],['do','eo','fo'],['go','ho','io'],['ao','eo','io'],['go','eo','co']];
 body.addEventListener('click', event => {
   console.log(event)
   var tile = event.target
