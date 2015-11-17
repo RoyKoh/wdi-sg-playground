e9358b5b51fa7870ff2613ab93660c59a8ceae0c
RoyKoh <i168168@yahoo.com.sg>
2015-11-17 15:24:05 +0800

    Project1 Readme Player1 Player2 Page Up

---
+++ project1/proj1.js
@@ -0,0 +1,29 @@
+var qnbank = [[1, 'Qn 1', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'C'],
+[2, 'Qn 2', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'B'],
+[3, 'Qn 3', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'A'],
+[4, 'Qn 4', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'B'],
+[5, 'Qn 5', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'C'],
+[6, 'Qn 6', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'D'],
+[7, 'Qn 7', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'D'],
+[8, 'Qn 8', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'B'],
+[9, 'Qn 9', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'B'],
+[10, 'Qn 10', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'A'],
+[11, 'Qn 11', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'C'],
+[12, 'Qn 12', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'B'],
+[13, 'Qn 13', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'A'],
+[14, 'Qn 14', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'B'],
+[15, 'Qn 15', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'C'],
+[16, 'Qn 16', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'C'],
+[17, 'Qn 17', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'D'],
+[18, 'Qn 18', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'D'],
+[19, 'Qn 19', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'A'],
+[20, 'Qn 20', 'A', 'XXXX', 'B', 'XXXX', 'C', 'XXXX', 'D', 'XXXX', 'A']]
+// Math.random.floor()*10 random number between 1-10 integer
+// var teams = ['Bruins', 'Cal Bears', 'Ravens', 'Ducks'];
+// for (var i = 0; i < teams.length; i++) {
+//   console.log(teams[i]);
+// }
+// var teams = ['Bruins', 'Cal Bears', 'Ravens', 'Ducks'];
+// teams.forEach(function(el) {
+//    console.log(el);
+// });
